42c6620bca8d56208cf5c03688725d41426136ef
orang <897444460@qq.com>
2022-09-26 09:44:57 +0800

    second commit

--- src/main/java/com/xtu/edu/GitTest.java
+++ src/main/java/com/xtu/edu/GitTest.java
@@ -3,5 +3,6 @@ package com.xtu.edu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hellow git2!");
     }
 }
